28fecdf510c06672a5ca6cebabe011086ee4fed8
Vitor Bonelli <bonelli5.vitor@gmail.com>
2024-02-06 03:55:46 -0300

    feat: add grid components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,41 @@
+import { Box, Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
 import Navbar from "../componentes/Navbar";
 
 export default function Home() {
   return (
-    <Navbar/>
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.100">
+        <GridItem
+            as="aside"
+            colSpan="1"
+            bg="purple.400"
+            minHeight="100vh"
+        >
+            <span>sidebar</span>
+        </GridItem>
+
+        <GridItem
+            as="main"
+            colSpan="5"
+            p="40px"
+        >
+            <Navbar/>
+            <SimpleGrid columns={4} spacing={5} minChildWidth="250px">
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+                <Box bg="white" h="200px" border="1px solid"></Box>
+            </SimpleGrid>
+        </GridItem>
+    </Grid>
   )
 }
